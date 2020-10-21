0a8c19ab94986e7433d953235e7aee7d565ef997
nab5m <sk990420@naver.com>
2020-10-22 00:39:05 +0900

    postgresql 설정

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -1,1 +1,14 @@
 from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'herren_prod',
+        'USER': 'herren_pord',
+        'PASSWORD': 'nab5m!_!',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
