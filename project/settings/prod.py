268d971e5e0664c1073974b477a78287cfd1fbaf
nab5m <sk990420@naver.com>
2020-10-22 00:41:27 +0900

    오타 수정

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -5,7 +5,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'herren_prod',
-        'USER': 'herren_pord',
+        'USER': 'herren_prod',
         'PASSWORD': 'nab5m!_!',
         'HOST': 'localhost',
         'PORT': '5432',
